5dc2e9f29bcb7a754fecdf9520363ec9e4602083
Michael Freyer <michael.freyer@barmer.de>
2026-04-07 12:48:29 +0200

    feat: firebase setup – firebase_options.dart, firestore.rules, main.dart initialized

--- haken_dran/lib/main.dart
+++ haken_dran/lib/main.dart
@@ -1,15 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'app.dart';
 import 'features/onboarding/onboarding_controller.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Firebase-Initialisierung folgt nach `flutterfire configure`
-  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   final prefs = await SharedPreferences.getInstance();
 
